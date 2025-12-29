aff18ee57638e35e05d1b016fa763ccbd987d141
김경빈 <154494689+kyeongb-bin@users.noreply.github.com>
2025-12-30 02:51:32 +0900

    [Design] Tailwind 기반 디자인 시스템 초기 세팅 (#2)
    * feat: Tailwind CSS v4 디자인 시스템 설정 추가
    
    * feat: Drop shadow 및 Radius 디자인 토큰 추가
    
    * feat: Pretendard 폰트 추가 및 Shadow 변수명 개선
    
    * design: 기본 레이아웃 설정
    
    ---------
    
    Co-authored-by: limtjdghks <epaktldk0203@gmail.com>

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router"
 
 export const Layout = () => {
     return (
-        <div>
-            <Outlet />
-        </div>
-    )
+		<div className='w-full max-w-main mx-auto px-gutter'>
+			<Outlet />
+		</div>
+	)
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,9 @@
 const MainPage = () => {
-    return (
-        <div>
-            <h1>MainPage</h1>
-        </div>
-    )
+	return (
+		<div>
+			<h1 className='heading-1 text-semantic-500 bg-semantic-200 p-4 rounded-100 shadow-brand'>MainPage</h1>
+		</div>
+	)
 }
 
 export default MainPage
