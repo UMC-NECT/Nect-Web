d19ab749d8a7a633b9414b1873ba6f4fb631a3e3
minseo <alstj09162@gmail.com>
2026-01-06 02:05:46 +0900

    [FEAT] 온보딩 공통 컴포넌트 세팅 (#9)

---
+++ src/components/common/BackButton.tsx
@@ -0,0 +1,22 @@
+import BackButtonIcon from '../../assets/icons/signup/back-button.svg?react'
+
+interface IBackButton {
+	onClick?: () => void
+}
+
+const BackButton = ({ onClick }: IBackButton) => {
+	return (
+		<>
+			<button
+				type='button'
+				onClick={onClick}
+				className='max-w-11.5 p-3.25 border rounded-full border-neutral-200 bg-neutral-50 text-neutral-600 hover:cursor-pointer hover:bg-neutral-200 duration-300 ease-in-out'
+				aria-label='뒤로가기'
+			>
+				<BackButtonIcon />
+			</button>
+		</>
+	)
+}
+
+export default BackButton
