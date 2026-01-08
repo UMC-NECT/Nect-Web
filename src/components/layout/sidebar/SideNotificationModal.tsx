a468b57a2c44ce9638f524d84d26b1c317f4ac82
kyeong-bin <kyeongb0315@gmail.com>
2026-01-09 00:31:05 +0900

    feat: 팀 작업실 모드에서 사이드바 조건부 렌더링 및 파일 구조 개선

---
+++ src/components/layout/sidebar/SideNotificationModal.tsx
@@ -0,0 +1,72 @@
+import { NotificationItem } from '@/components/common/NotificationItem'
+
+const SideNotificationModal = () => {
+	return (
+		<div className='w-[360px] bg-white rounded-2xl border border-neutral-200 z-50 overflow-hidden shadow-drop-neutral-1'>
+			<div className='notification-scrollbar h-[500px] overflow-y-auto pl-4'>
+				<NotificationItem
+					notification={{
+						id: 1,
+						category: 'NECT 플랫폼',
+						title: 'PM 시루님이 다음에서 나를 @언급함',
+						description: '"예원님 이거 수정사항 생겨서 여기에 첨리해두었어요!"',
+						time: '25.11.26',
+						isRead: false,
+					}}
+				/>
+				<NotificationItem
+					notification={{
+						id: 2,
+						category: 'NECT 플랫폼',
+						title: '새로운 위크 미션이 등록되었습니다.',
+						description: '"Misson 2 온보딩 페이지 만들기"',
+						time: '25.11.24',
+						isRead: true,
+					}}
+				/>
+				<NotificationItem
+					notification={{
+						id: 2,
+						category: 'NECT 플랫폼',
+						title: '새로운 위크 미션이 등록되었습니다.',
+						description: '"Misson 2 온보딩 페이지 만들기"',
+						time: '25.11.24',
+						isRead: true,
+					}}
+				/>
+				<NotificationItem
+					notification={{
+						id: 2,
+						category: 'NECT 플랫폼',
+						title: '새로운 위크 미션이 등록되었습니다.',
+						description: '"Misson 2 온보딩 페이지 만들기"',
+						time: '25.11.24',
+						isRead: true,
+					}}
+				/>
+				<NotificationItem
+					notification={{
+						id: 2,
+						category: 'NECT 플랫폼',
+						title: '새로운 위크 미션이 등록되었습니다.',
+						description: '"Misson 2 온보딩 페이지 만들기"',
+						time: '25.11.24',
+						isRead: true,
+					}}
+				/>
+				<NotificationItem
+					notification={{
+						id: 2,
+						category: 'NECT 플랫폼',
+						title: '새로운 위크 미션이 등록되었습니다.',
+						description: '"Misson 2 온보딩 페이지 만들기"',
+						time: '25.11.24',
+						isRead: true,
+					}}
+				/>
+			</div>
+		</div>
+	)
+}
+
+export default SideNotificationModal
