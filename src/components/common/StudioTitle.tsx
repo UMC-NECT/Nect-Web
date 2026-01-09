d05f54a4025bc25b38f723991f40f31d92df5aab
limtjdghks <epaktldk0203@gmail.com>
2026-01-10 08:31:37 +0900

    feat: StudioTitle 컴포넌트 구현

---
+++ src/components/common/StudioTitle.tsx
@@ -0,0 +1,24 @@
+import CalendarIcon from '@/assets/icons/week-mission/calender.svg?react'
+
+interface StudioTitleProps {
+	title: string
+	description: string
+}
+
+const StudioTitle = ({ title, description }: StudioTitleProps) => {
+	return (
+		<div className='flex items-center gap-6'>
+				<div className='flex items-center justify-center p-2.5 bg-neutral-50 rounded-xl w-[72px] h-[72px]'>
+					<CalendarIcon className='w-[50px] h-[50px]' />
+				</div>
+				<div className='flex flex-col gap-2'>
+					<h1 className='heading-2 font-bold text-neutral-900'>{title}</h1>
+					<p className='title-3 font-medium text-neutral-600'>
+						{description}
+					</p>
+				</div>
+			</div>
+	)
+}
+
+export default StudioTitle
