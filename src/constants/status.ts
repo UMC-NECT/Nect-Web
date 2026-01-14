8aab501ff9b6c341ce6cfc8ca88767764abb3e28
limtjdghks <epaktldk0203@gmail.com>
2026-01-14 20:42:59 +0900

    fix: status 설정 변경

---
+++ src/constants/status.ts
@@ -0,0 +1,18 @@
+export const STATUS = {
+    planning: {
+        text: '진행 전',
+        dotColor: 'bg-status-complete', // 파란색
+    },
+    in_progress: {
+        text: '진행 중',
+        dotColor: 'bg-status-progress', // 노란색
+    },
+    completed: {
+        text: '완료',
+        dotColor: 'bg-status-success', // 초록색
+    },
+    backlog: {
+        text: '백로그',
+        dotColor: 'bg-status-info', // 회색
+    },
+}
