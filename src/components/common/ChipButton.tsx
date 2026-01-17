a3fe90e6adc100e1461ad20eed6a7fda479f82ff
minseeeeo <alstj09162@gmail.com>
2026-01-17 20:39:33 +0900

    design: 칩버튼 선택상태 폰트 두께 조정

--- src/components/common/ChipButton.tsx
+++ src/components/common/ChipButton.tsx
@@ -14,7 +14,7 @@ const ChipButton = ({ text, isChecked = false, onClick, className }: IChipButton
 			className={cn(
 				'title-3 px-5 py-2.5 w-73 border-2 rounded-12 hover:bg-primary-400-normal duration-200 ease-in-out',
 				isChecked
-					? 'text-neutral-50 bg-primary-400-normal border-primary-400-normal'
+					? 'text-neutral-50 font-semibold bg-primary-400-normal border-primary-400-normal'
 					: 'text-neutral-700 bg-primary-50-light border-primary-300-light hover:text-neutral-50 hover:border-primary-400-normal',
 				className
 			)}
