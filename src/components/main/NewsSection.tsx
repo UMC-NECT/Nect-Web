6d23e1971704d24bc234df3964d64a8d821f74aa
Dawon-Y <yang20401@naver.com>
2026-02-05 01:42:18 +0900

    fix: 홈화면 수정

--- src/components/main/NewsSection.tsx
+++ src/components/main/NewsSection.tsx
@@ -87,7 +87,7 @@ const NewsSection = () => {
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
                     </svg>
-                </div>
+                </div>          
                 <div className="news-custom-next absolute right-[-50px] top-[138px] z-10 w-12 h-12 flex items-center justify-center cursor-pointer transition-all hover:bg-neutral-100 hover:rounded-full text-neutral-700">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
