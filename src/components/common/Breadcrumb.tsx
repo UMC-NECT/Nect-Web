9ad3bd1967a3bbaaf18bad31506218db36434a6d
Dawon-Y <yang20401@naver.com>
2026-02-05 20:43:31 +0900

    fix: 코드 수정

--- src/components/common/Breadcrumb.tsx
+++ src/components/common/Breadcrumb.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link } from "react-router";
 
 interface BreadcrumbItem {
@@ -13,10 +14,9 @@ const Breadcrumb = ({ items }: BreadcrumbProps) => {
     return (
         <nav className="text-[14px] text-neutral-400 gap-1 flex items-center">
             {items.map((item, index) => (
-                <>
+                <React.Fragment key={index}>
                     {item.path ? (
                         <Link 
-                            key={`link-${index}`}
                             to={item.path}
                             className="hover:text-neutral-600 hover:font-semibold cursor-pointer transition-colors"
                         >
@@ -24,18 +24,17 @@ const Breadcrumb = ({ items }: BreadcrumbProps) => {
                         </Link>
                     ) : (
                         <span 
-                            key={`span-${index}`}
                             className="hover:text-neutral-600 hover:font-semibold cursor-pointer transition-colors"
                         >
                             {item.label}
                         </span>
                     )}
                     {index < items.length - 1 && (
-                        <span key={`separator-${index}`} className="text-neutral-400">
+                        <span className="text-neutral-400">
                             {' > '}
                         </span>
                     )}
-                </>
+                </React.Fragment>
             ))}
         </nav>
     );
