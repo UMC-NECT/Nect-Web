2a92c6f371d88efabb29e1c6d59f299d74e5b3e7
kyeong-bin <kyeongb0315@gmail.com>
2026-02-07 04:15:25 +0900

    fix: 게시판 레이아웃 개선

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -355,7 +355,7 @@ const BoardPage = () => {
 	const currentItems = boardItems.slice(startIndex, endIndex)
 
 	return (
-		<div className="flex flex-col w-full mx-auto px-6 py-8 gap-[30px]">
+		<div className="flex flex-col w-full mx-auto px-6 py-[64px] gap-[30px]">
 			<ContentHeader
 				title="게시판"
 				description="팀 전용 게시판입니다. 공지사항, 회의록, 업무 보고 등을 공유할 수 있습니다."
