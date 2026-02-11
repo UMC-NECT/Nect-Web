4a788231f0fe7cfb8b85f1a025584107fb968b0a
limtjdghks <epaktldk0203@gmail.com>
2026-02-12 00:07:18 +0900

    design: 미션 칩 수정

--- src/components/mission-modal/MissionTagChip.tsx
+++ src/components/mission-modal/MissionTagChip.tsx
@@ -5,7 +5,7 @@ interface MissionTagChipProps {
 const MissionTagChip = ({ missionNumber }: MissionTagChipProps) => {
     return (
         <div className='bg-primary-150-light rounded-6 py-0.5 px-2 w-20 h-6 shadow-drop-neutral-2 hover:cursor-pointer'>
-            <p className='button-1 font-medium text-neutral-700 text-center'>
+            <p className='button-1 font-medium text-neutral-700 text-center whitespace-nowrap'>
                 Mission {missionNumber}
             </p>
         </div>
